16cd6f803135398309930aae79b08290017aba8b
Franco Marquez <20191218@aloe.ulima.edu.pe>
2023-04-03 11:12:34 -0500

    Initial Commit

---
+++ swipeableCards/swipeableCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeableCardsApp.swift
+//  swipeableCards
+//
+//  Created by Franco Marquez on 3/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swipeableCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
